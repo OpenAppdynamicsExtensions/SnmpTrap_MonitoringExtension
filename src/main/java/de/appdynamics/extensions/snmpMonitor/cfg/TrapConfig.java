cf833132fa1d2ab86a2deae673baced250249567
Stefan Marx <smarx@marxworld.net>
2015-04-21 18:14:21 +0200

    fixed unmarshalling bug (empty constructor and setter)

--- src/main/java/de/appdynamics/extensions/snmpMonitor/cfg/TrapConfig.java
+++ src/main/java/de/appdynamics/extensions/snmpMonitor/cfg/TrapConfig.java
@@ -4,8 +4,13 @@ package de.appdynamics.extensions.snmpMonitor.cfg;
  * Created by stefan.marx on 17.04.15.
  */
 public class TrapConfig {
+    public void setOid(String oid) {
+        _oid = oid;
+    }
+
     private String _oid;
 
+    public TrapConfig() {}
     public TrapConfig(String trapOid) {
         _oid = trapOid;
 
