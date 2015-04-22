463a9179d88ba945db4556b2ce24b8557aaf1497
rajatlocal <rajatparida86@gmail.com>
2015-04-22 15:57:27 +0200

    Changed createmockupconfig to include trap name

--- src/test/java/de/appdynamics/extensions/ConfigTest.java
+++ src/test/java/de/appdynamics/extensions/ConfigTest.java
@@ -70,22 +70,22 @@ public class ConfigTest {
 
         // SNMP Trap End Point
         SnmpEndpointDefinition ep = new SnmpEndpointDefinition();
-        ep.setListenerPort(9999);
+        ep.setListenerPort(1026);
         ep.setListenerAddress("0.0.0.0");
         ep.setSNMPv1Enable(true);
         ep.setSNMPV2Enable(true);
         ep.setSNMPV3Enable(false);
         cfg.setEndpointConfig(ep);
 
-        TrapConfig trap = new TrapConfig(".1.2.3.2.3.4.2.3.4.2.1.2500");
+        TrapConfig trap = new TrapConfig("1.2.3.2.3.4.2.3.4.2.1.2500", "Met2500");
         cfg.addTrapConfig(trap);
 
 
-        trap = new TrapConfig(".1.2.3.2.3.4.2.3.4.2.1.2501");
+        trap = new TrapConfig("1.2.3.2.3.4.2.3.4.2.1.2501" , "Met2501");
         cfg.addTrapConfig(trap);
 
 
-        trap = new TrapConfig(".1.2.3.2.3.4.2.3.4.2.1.2502");
+        trap = new TrapConfig("1.2.3.2.3.4.2.3.4.2.1.2502" , "Met2502");
         cfg.addTrapConfig(trap);
 
         return cfg;
