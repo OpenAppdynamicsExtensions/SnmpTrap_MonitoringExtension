910fd04125f53552daf7535d64423f0195cc6aa5
rajatlocal <rajatparida86@gmail.com>
2015-04-22 11:32:23 +0200

    Cleanup and commenting

--- src/main/java/de/appdynamics/extensions/snmpMonitor/cfg/SnmpTrapMonitorConfig.java
+++ src/main/java/de/appdynamics/extensions/snmpMonitor/cfg/SnmpTrapMonitorConfig.java
@@ -7,6 +7,10 @@ import java.util.Map;
  * Created by stefan.marx on 17.04.15.
  */
 public class SnmpTrapMonitorConfig {
+
+    private Map<String, TrapConfig> _traps = new Hashtable<String, TrapConfig>();
+    private SnmpEndpointDefinition _endpointConfig;
+
     public Map<String, TrapConfig> getTraps() {
         return _traps;
     }
@@ -15,21 +19,18 @@ public class SnmpTrapMonitorConfig {
         _traps = traps;
     }
 
-    private Map<String, TrapConfig> _traps = new Hashtable<String, TrapConfig>();
-    private SnmpEndpointDefinition _endpointConfig;
 
-    public void addTrapConfig(TrapConfig trap) {
-        if (!_traps.containsKey(trap.getOid())) {
-            _traps.put(trap.getOid(),trap);
-        }
-
+    public SnmpEndpointDefinition getEndpointConfig() {
+        return _endpointConfig;
     }
 
     public void setEndpointConfig(SnmpEndpointDefinition endpointConfig) {
         _endpointConfig = endpointConfig;
     }
 
-    public SnmpEndpointDefinition getEndpointConfig() {
-        return _endpointConfig;
+    public void addTrapConfig(TrapConfig trap) {
+        if (!_traps.containsKey(trap.getOid())) {
+            _traps.put(trap.getOid(),trap);
+        }
     }
 }
