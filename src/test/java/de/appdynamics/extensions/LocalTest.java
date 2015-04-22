9ad4aecb427de6260e91afeaf817bc2af4d7fb53
rajatlocal <rajatparida86@gmail.com>
2015-04-22 15:58:48 +0200

    Removed createmockupconfig

--- src/test/java/de/appdynamics/extensions/LocalTest.java
+++ src/test/java/de/appdynamics/extensions/LocalTest.java
@@ -16,33 +16,6 @@ import java.util.Map;
  * Created by stefan.marx on 17.04.15.
  */
 public class LocalTest {
-    private static SnmpTrapMonitorConfig createMockupConfiguration() {
-
-        SnmpTrapMonitorConfig cfg = new SnmpTrapMonitorConfig();
-
-        // SNMP Trap End Point
-        SnmpEndpointDefinition ep = new SnmpEndpointDefinition();
-        ep.setListenerPort(9999);
-        ep.setListenerAddress("0.0.0.0");
-        ep.setSNMPv1Enable(true);
-        ep.setSNMPV2Enable(true);
-        ep.setSNMPV3Enable(false);
-
-        cfg.setEndpointConfig(ep);
-
-        TrapConfig trap = new TrapConfig(".1.2.3.2.3.4.2.3.4.2.1.2500");
-        cfg.addTrapConfig(trap);
-
-
-        trap = new TrapConfig(".1.2.3.2.3.4.2.3.4.2.1.2501");
-        cfg.addTrapConfig(trap);
-
-
-        trap = new TrapConfig(".1.2.3.2.3.4.2.3.4.2.1.2502");
-        cfg.addTrapConfig(trap);
-
-        return cfg;
-    }
 
     public static void main(String[] args) {
        /* // Read the local config File
