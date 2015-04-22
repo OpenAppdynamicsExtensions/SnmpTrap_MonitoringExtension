d6e13706631776569c7946b9f6a6a77c91d2b8e5
rajatlocal <rajatparida86@gmail.com>
2015-04-22 16:01:33 +0200

    Added name field for a trap

--- src/main/java/de/appdynamics/extensions/snmpMonitor/cfg/TrapConfig.java
+++ src/main/java/de/appdynamics/extensions/snmpMonitor/cfg/TrapConfig.java
@@ -4,19 +4,29 @@ package de.appdynamics.extensions.snmpMonitor.cfg;
  * Created by stefan.marx on 17.04.15.
  */
 public class TrapConfig {
-    public void setOid(String oid) {
-        _oid = oid;
-    }
 
     private String _oid;
+    private String _name;
 
     public TrapConfig() {}
-    public TrapConfig(String trapOid) {
+    public TrapConfig(String trapOid, String trapName) {
         _oid = trapOid;
-
+        set_name(trapName);
     }
 
     public String getOid() {
         return _oid;
+    }
+
+    public void setOid(String oid) {
+        _oid = oid;
+    }
+
+    public String get_name() {
+        return _name;
+    }
+
+    public void set_name(String _name) {
+        this._name = _name;
     }
 }
