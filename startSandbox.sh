3ab14a74fb1bb943260eaf8486d3593acdb5eed2
rajatlocal <rajatparida86@gmail.com>
2015-05-05 09:25:41 +0200

    Specified the controller port and host name

--- startSandbox.sh
+++ startSandbox.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-JAVA_OPTIONS="-Dappdynamics.agent.applicationName=APPDEX_LBG
--Dappdynamics.controller.hostName=${controllerHost-localhost}
+JAVA_OPTIONS="-Dappdynamics.agent.applicationName=SNMPTrapMonitor
+-Dappdynamics.controller.hostName=${controllerHost-172.199.0.5}
 -Dappdynamics.controller.port=${controllerPort-8090}
--Dappdynamics.agent.tierName=APPDEX
+-Dappdynamics.agent.tierName=SNMP
 -Dappdynamics.agent.nodeName=NodeUTIL
+-Dappdynamics.agent.uniqueHostId=SNMPSandbox
 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
 
 java $JAVA_OPTIONS -jar ./build/sandbox/agent/machineagent.jar
