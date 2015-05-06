876a38d4c086e8ee0d6d00b472a76c02f63ca2d5
rajatlocal <rajatparida86@gmail.com>
2015-05-06 22:39:15 +0200

    Removed extra separator from Metric path and logged the metric name

--- src/main/java/de/appdynamics/extensions/snmpMonitor/AppDMachineAgentMetricConsumer.java
+++ src/main/java/de/appdynamics/extensions/snmpMonitor/AppDMachineAgentMetricConsumer.java
@@ -2,9 +2,9 @@ package de.appdynamics.extensions.snmpMonitor;
 
 import com.singularity.ee.agent.commonservices.metricgeneration.metrics.MetricAggregatorType;
 import com.singularity.ee.agent.systemagent.api.MetricWriter;
-import com.singularity.ee.agent.systemagent.api.AManagedMonitor;
 import com.singularity.ee.controller.api.constants.MetricClusterRollupType;
 import com.singularity.ee.controller.api.constants.MetricTimeRollupType;
+import org.apache.log4j.Logger;
 
 
 /**
@@ -12,11 +12,11 @@ import com.singularity.ee.controller.api.constants.MetricTimeRollupType;
  */
 public class AppDMachineAgentMetricConsumer implements SNMPMetricConsumer{
 
-    private static final String metricPrefix = "Custom Metrics|SNMPTraps|";
+    private static final String METRIC_PREFIX = "Custom Metrics|SNMPTraps";
     private static final String METRIC_SEP = "|";
     public static final String EVENTS = "events";
     private SnmpAgent _snmpAgent;
-
+    private static Logger logger = Logger.getLogger(SNMPMetricConsumer.class);
     public AppDMachineAgentMetricConsumer(SnmpAgent snmpAgent) {
 
         _snmpAgent = snmpAgent;
@@ -29,8 +29,16 @@ public class AppDMachineAgentMetricConsumer implements SNMPMetricConsumer{
 
     @Override
     public synchronized void reportTrap(String name) {
-        MetricWriter writer = _snmpAgent.getMetricWriter(getEventsMetricPath(name), MetricAggregatorType.SUM.toString(),
-                MetricTimeRollupType.SUM.toString(), MetricClusterRollupType.COLLECTIVE.toString());
+        /*MetricWriter writer = _snmpAgent.getMetricWriter(getEventsMetricPath(name),
+                                                        MetricAggregatorType.SUM.toString(),
+                                                        MetricTimeRollupType.SUM.toString(),
+                                                        MetricClusterRollupType.COLLECTIVE.toString());*/
+        MetricWriter writer = _snmpAgent.getMetricWriter(getEventsMetricPath(name),
+                                                        MetricWriter.METRIC_AGGREGATION_TYPE_SUM,
+                                                        MetricWriter.METRIC_TIME_ROLLUP_TYPE_SUM,
+                                                        MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE);
+        //MetricWriter writer = _snmpAgent.getMetricWriter(getEventsMetricPath(name));
+        logger.debug("Uploading metric to controller: " + name);
         writer.printMetric("1");
     }
 
@@ -42,6 +50,6 @@ public class AppDMachineAgentMetricConsumer implements SNMPMetricConsumer{
 
 
     private String getEventsMetricPath(String trapName) {
-        return metricPrefix+METRIC_SEP+trapName+METRIC_SEP+ EVENTS;
+        return METRIC_PREFIX + METRIC_SEP + trapName + METRIC_SEP + EVENTS;
     }
 }
